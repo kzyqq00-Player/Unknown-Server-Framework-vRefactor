5a69c283b1900b4208051d081ea725a8a45b4d1f
commandwither <command_wither@hotmail.com>
2025-08-01 19:32:54 +0800

    指令分为/usf:manager、/usf:func，供管理员、成员使用

--- 无名氏插件V3_B/scripts/Functions/Command.js
+++ 无名氏插件V3_B/scripts/Functions/Command.js
@@ -1,74 +1,102 @@
 import * as mc from "@minecraft/server";
 import {
-  USFPlayer,
-  Log
+	USFPlayer,
+	Log
 } from "../API/API.js";
-import { UIManager } from "../UserInterfaces/init.js";
+import {
+	UIManager
+} from "../UserInterfaces/init.js";
 
 mc.system.beforeEvents.startup.subscribe((event) => {
-  event.customCommandRegistry.registerEnum("usf:function", ["get_owner", "tp", "manager", "reset_owner", "cd", "menu"]);
-  event.customCommandRegistry.registerCommand({
-    cheatsRequired: false,
-    description: "usf指令",
-    permissionLevel: 2,
-    name: "usf:func",
-    mandatoryParameters: [{
-      name: "usf:function",
-      type: "Enum"
-    }]
-  }, (source, arg) => {
-    switch(arg){
-      case "get_owner":
-        if(source.sourceType !== "Entity"){
-          return {
-            message: "发送者非实体",
-            status: 1
-          }
-          break;
-        };
-        if(source.sourceEntity.typeId !== "minecraft:player"){
-          return {
-            message: "发送者非玩家",
-            status: 1
-          }
-          break;
-        }
-        if(mc.world.getDynamicProperty("usf:owner") !== undefined){
-          return {
-            message: "设置失败，已有服主",
-            status: 1
-          }
-          break;
-        } else {
-          mc.world.setDynamicProperty("usf:owner", USFPlayer.getId(source.sourceEntity));
-          USFPlayer.opLevel.setLevel(source.sourceEntity, 5);
-          return {
-            message: "设置成功",
-            status: 0
-          }
-        }
-        break;
-      case "reset_owner":
-        if(source.sourceType === "Server" || USFPlayer.getId(source.sourceEntity) === mc.world.getDynamicProperty("usf:owner")){
-          mc.world.setDynamicProperty("usf:owner", undefined);
-          USFPlayer.opLevel.setLevel(source.sourceEntity, 0);
-          return {
-            message: "已重置服主",
-            status: 0
-          }
-        } else {
-          return {
-            message: "重置失败",
-            status: 1
-          }
-        }
-        break;
-      case "cd":
+	event.customCommandRegistry.registerEnum("usf:manager", ["get_owner", "reset_owner", "open"]);
+	event.customCommandRegistry.registerEnum("usf:func", ["cd", "menu", "tp"]);
+	event.customCommandRegistry.registerCommand({
+		cheatsRequired: false,
+		description: "usf管理员指令",
+		permissionLevel: 2,
+		name: "usf:manager",
+		mandatoryParameters: [{
+			name: "usf:manager",
+			type: "Enum"
+		}]
+	}, (source, arg) => {
+		switch (arg) {
+			case "get_owner":
+				if (source.sourceType !== "Entity") {
+					return {
+						message: "发送者非实体",
+						status: 1
+					}
+					break;
+				};
+				if (source.sourceEntity.typeId !== "minecraft:player") {
+					return {
+						message: "发送者非玩家",
+						status: 1
+					}
+					break;
+				}
+				if (mc.world.getDynamicProperty("usf:owner") !== undefined) {
+					return {
+						message: "设置失败，已有服主",
+						status: 1
+					}
+					break;
+				} else {
+					mc.world.setDynamicProperty("usf:owner", USFPlayer.getId(source.sourceEntity));
+					USFPlayer.opLevel.setLevel(source.sourceEntity, 5);
+					return {
+						message: "设置成功",
+						status: 0
+					}
+				}
+				break;
+			case "reset_owner":
+				if (source.sourceType === "Server" || USFPlayer.getId(source.sourceEntity) === mc.world.getDynamicProperty("usf:owner")) {
+					mc.world.setDynamicProperty("usf:owner", undefined);
+					USFPlayer.opLevel.setLevel(source.sourceEntity, 0);
+					return {
+						message: "已重置服主",
+						status: 0
+					}
+				} else {
+					return {
+						message: "重置失败",
+						status: 1
+					}
+				}
+				break;
+		}
+	});
+	event.customCommandRegistry.registerCommand({
+		cheatsRequired: false,
+		description: "usf指令",
+		permissionLevel: 0,
+		name: "usf:func",
+		mandatoryParameters: [{
+			name: "usf:func",
+			type: "Enum"
+		}]
+	}, (source, arg) => {
+		if (source.sourceType !== "Entity") {
+			return {
+				message: "发送者非实体",
+				status: 1
+			}
+		};
+		if (source.sourceEntity.typeId !== "minecraft:player") {
+			return {
+				message: "发送者非玩家",
+				status: 1
+			}
+		};
+		switch (arg) {
+			case "cd":
       case "menu":
         if(source?.sourceEntity){
           new (UIManager.getUI("mainGUI"))().sendToPlayer(source.sourceEntity);
         }
         break;
-    }
-  });
+		}
+	})
 });
